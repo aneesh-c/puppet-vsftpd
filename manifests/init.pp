743c94cc32b02cee83f44d2ed24ed973622379e6
Lukas Hejtmanek <xhejtman@gmail.com>
2019-03-28 16:34:54 +0100

    MODULE vsftpd: add letsencrypt

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,50 +16,54 @@
 #
 
 class vsftpd (
-  $package_name           = [ 'vsftpd' ],
-  $configfile             = $::vsftpd::params::configfile,
-  $template               = 'vsftpd/configfile.erb',
-  $anonymous_enable       = undef,
-  $local_enable           = undef,
-  $write_enable           = undef,
-  $local_umask            = undef,
-  $dirmessage_enable      = undef,
-  $xferlog_enable         = undef,
-  $connect_from_port_20   = undef,
-  $xferlog_std_format     = undef,
-  $chroot_local_user      = undef,
-  $listen                 = undef,
-  $listen_ipv6            = undef,
-  $pam_service_name       = undef,
-  $userlist_enable        = undef,
-  $tcp_wrappers           = undef,
-  $use_localtime          = undef,
-  $secure_chroot_dir      = undef,
-  $rsa_cert_file          = undef,
-  $rsa_private_key_file   = undef,
-  $ssl_enable             = undef,
-  $allow_writeable_chroot = undef,
-  $pasv_enable            = undef,
-  $pasv_min_port          = undef,
-  $pasv_max_port          = undef,
-  $pasv_address           = undef,
-  $allow_anon_ssl         = undef,
-  $force_local_data_ssl   = undef,
-  $force_local_logins_ssl = undef,
-  $ssl_tlsv1              = undef,
-  $ssl_sslv2              = undef,
-  $ssl_sslv3              = undef,
-  $require_ssl_reuse      = undef,
-  $ssl_ciphers            = undef,
-  $async_abor_enable      = undef,
-  $data_connection_timeout= undef,
-  $debug_ssl              = undef,
-  $force_dot_files        = undef,
-  $idle_session_timeout   = undef,
-  $passwd_chroot_enable   = undef,
-  $session_support        = undef,
-  $xferlog_file           = undef,
-  $manage_service         = true,
+  $package_name            = [ 'vsftpd' ],
+  $configfile              = $::vsftpd::params::configfile,
+  $template                = 'vsftpd/configfile.erb',
+  $anonymous_enable        = undef,
+  $local_enable            = undef,
+  $write_enable            = undef,
+  $local_umask             = undef,
+  $dirmessage_enable       = undef,
+  $xferlog_enable          = undef,
+  $connect_from_port_20    = undef,
+  $xferlog_std_format      = undef,
+  $chroot_local_user       = undef,
+  $listen                  = undef,
+  $listen_ipv6             = undef,
+  $pam_service_name        = undef,
+  $userlist_enable         = undef,
+  $tcp_wrappers            = undef,
+  $use_localtime           = undef,
+  $secure_chroot_dir       = undef,
+  $rsa_cert_file           = undef,
+  $rsa_private_key_file    = undef,
+  $ssl_enable              = undef,
+  $allow_writeable_chroot  = undef,
+  $pasv_enable             = undef,
+  $pasv_min_port           = undef,
+  $pasv_max_port           = undef,
+  $pasv_address            = undef,
+  $allow_anon_ssl          = undef,
+  $force_local_data_ssl    = undef,
+  $force_local_logins_ssl  = undef,
+  $ssl_tlsv1               = undef,
+  $ssl_sslv2               = undef,
+  $ssl_sslv3               = undef,
+  $require_ssl_reuse       = undef,
+  $ssl_ciphers             = undef,
+  $async_abor_enable       = undef,
+  $data_connection_timeout = undef,
+  $debug_ssl               = undef,
+  $force_dot_files         = undef,
+  $idle_session_timeout    = undef,
+  $passwd_chroot_enable    = undef,
+  $session_support         = undef,
+  $xferlog_file            = undef,
+  $manage_service          = true,
+  Boolean $letsencryptcert = false,
+  Optional[String] $lemail = undef,
+  Optional[String] $lename = undef,
+  Optional[String] $lecron = 'present',
 ) inherits ::vsftpd::params {
   package { $package_name: ensure => installed }
   file { $configfile:
@@ -67,11 +71,39 @@ class vsftpd (
     backup  => '.backup',
     content => template($template),
   }
+  if $letsencryptcert == true {
+    if $lemail { 
+      class {'letsencrypt':
+        email => $lemail,
+        config => {
+          server => 'https://acme-staging.api.letsencrypt.org/directory',
+        },
+      }
+      letsencrypt::certonly { $facts['fqdn']:        
+        domains              => delete_undef_values([$facts['fqdn'], $lename]),
+        ensure_cron          => $lecron,
+        cron_hour            => '0',
+        cron_minute          => '30',
+        cron_success_command => '/usr/sbin/service vsftpd restart',
+        suppress_cron_output => true,
+      }
+    }
+  }
   if $manage_service {
-    if $::osfamily == 'RedHat' {
-      service { 'vsftpd':
-        require => Package[$package_name],
-        enable  => true,
+    case $facts['osfamily'] {
+      'RedHat': {
+         service { 'vsftpd':
+           require => Package[$package_name],
+           enable  => true,
+         }
+      }
+      'Debian': {
+         if $facts['operatingsystemmajrelease'] > 8 {
+            service { 'vsftpd':
+              require => Package[$package_name],
+              enable  => true,
+            }
+         }
       }
     }
   }
